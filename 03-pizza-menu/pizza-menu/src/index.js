995325c685194e8c50bccf5902b33d0792654392
ayan878 <ayanraza197@gmail.com>
2024-01-02 02:01:46 +0530

    pizza-menu

--- 03-pizza-menu/pizza-menu/src/index.js
+++ 03-pizza-menu/pizza-menu/src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM  from 'react-dom/client';
 
-const App=()=>(<h1>Hello React!</h1>);
+const App = () => (
+  <div>
+    <h1>Hello React!</h1>
+    <Pizza />
+  </div>
+);
+const Pizza=()=><h2>Pizza</h2>;
 const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(<React.StrictMode><App/></React.StrictMode>);
