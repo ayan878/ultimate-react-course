1ddbd291327446427402084f922f96c280603fe9
ayan878 <ayanraza197@gmail.com>
2024-01-02 01:41:57 +0530

    pizza-Menu

--- 03-pizza-menu/pizza-menu/src/index.js
+++ 03-pizza-menu/pizza-menu/src/index.js
@@ -2,6 +2,5 @@ import React from 'react';
 import ReactDOM  from 'react-dom/client';
 
 const App=()=> <h1>Hello React!</h1>
-
 const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(<React.StrictMode><App/></React.StrictMode>);
