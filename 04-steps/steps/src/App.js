fb7aa8e2c3a111da3de75291a0d1bd397e4e6703
ayan878 <ayanraza197@gmail.com>
2024-01-04 16:19:48 +0530

    Steps

--- 04-steps/steps/src/App.js
+++ 04-steps/steps/src/App.js
@@ -1,10 +1,51 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { useState } from "react";
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+export default function App() {
+  // let step=1;
+  const [step, setStep] = useState(1);
+  const handleNext = () => {
+    if (step < 3) {
+      setStep((s) => s + 1);
+      // setStep((s) => s + 1);
+    }
+  };
+  const handlePreviouse = () => {
+    if (step > 1) {
+      setStep((s) => s - 1);
+      // setStep((s) => s + 1);
+    }
+  };
+  return (
+    <div class="steps">
+      <div class="numbers">
+        <div class={`${step === 1 ? "active" : ""}`}>1</div>
+        <div class={`${step === 2 ? "active" : ""}`}>2</div>
+        <div class={`${step === 3 ? "active" : ""}`}>3</div>
+      </div>
+      <p class="message">
+        Step {step}:{messages[step - 1]}
+      </p>
+      <div class="buttons">
+        <button
+          class="previous"
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handlePreviouse}
+        >
+          Previous
+        </button>
+        <button
+          class="next"
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handleNext}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+}
--- 04-steps/steps/src/index.js
+++ 04-steps/steps/src/index.js
@@ -1,42 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import App from "./App";
 
- const messages = [
-   "Learn React ⚛️",
-   "Apply for jobs 💼",
-   "Invest your new income 🤑",
- ];
-
-const App = () => (<div>
-  {/* <Header/> */}
-<Container/></div>);
-
-
-// const Header = () => <h1>hello World</h1>;
-const Container = () =>
- { return(
-    <div class="steps">
-      <div class="numbers">
-        <div class="step-1">1</div>
-        <div class="step-2">2</div>
-        <div class="step-3">3</div>
-      </div>
-
-      <p class="message"></p>
-
-      <div class="buttons">
-        <button class="previous">Previous</button>
-        <button class="next">Next</button>
-      </div>
-    </div>
-  );
- };
-
- const message=(props)=>{
-
-
- };
-
-
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
