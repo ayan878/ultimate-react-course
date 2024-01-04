ace665c55aa3b91d5f85605d17098cb2d3baa0e1
ayan878 <ayanraza197@gmail.com>
2024-01-04 17:47:57 +0530

    Steps

--- 04-steps/steps/src/App.js
+++ 04-steps/steps/src/App.js
@@ -6,29 +6,27 @@ const messages = [
 ];
 
 export default function App() {
-  // let step=1;
   const [step, setStep] = useState(1);
+  const [isOpen,setIsOpen]=useState(true);
   const handleNext = () => {
     if (step < 3) {
       setStep((s) => s + 1);
-      // setStep((s) => s + 1);
     }
   };
   const handlePreviouse = () => {
     if (step > 1) {
       setStep((s) => s - 1);
-      // setStep((s) => s + 1);
     }
   };
   return (
     <div class="steps">
       <div class="numbers">
-        <div class={`${step === 1 ? "active" : ""}`}>1</div>
-        <div class={`${step === 2 ? "active" : ""}`}>2</div>
-        <div class={`${step === 3 ? "active" : ""}`}>3</div>
+        <div class={step === 1 ? "active" : ""}>1</div>
+        <div class={step === 2 ? "active" : ""}>2</div>
+        <div class={step === 3 ? "active" : ""}>3</div>
       </div>
       <p class="message">
-        Step {step}:{messages[step - 1]}
+        Step {step}: {messages[step - 1]}
       </p>
       <div class="buttons">
         <button
