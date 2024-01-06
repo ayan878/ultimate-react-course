4e8b7055b783af12fca22f6aea8722db40bd45bc
ayan878 <ayanraza197@gmail.com>
2024-01-06 19:56:46 +0530

    travel-list

--- 05-travel-list/travel-list/src/App.js
+++ 05-travel-list/travel-list/src/App.js
@@ -1,8 +1,28 @@
 export default function App() {
-  function Logo() {
-    return <h1>Far Away🎉</h1>;
-  }
-  function Form() {}
-  function PackingList() {}
-  function Stats() {}
+  return <div className="app">
+    <Logo/>
+    <Form/>
+    <PackingList/>
+    <Stats/>
+  </div>
+}
+function Logo() {
+  return <h1>Far Away🎉</h1>;
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h1>What is you need for your trip </h1>
+    </div>
+  );
+}
+function PackingList() {
+  return <div className="list">List</div>;
+}
+function Stats() {
+  return (
+    <footer>
+      <em>You have X items on your lsit,and you already packed X (X%)</em>
+    </footer>
+  );
 }
