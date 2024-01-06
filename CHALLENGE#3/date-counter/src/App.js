7af663be490952c2d3296ee0d1ab5cd021f174a4
ayan878 <ayanraza197@gmail.com>
2024-01-06 12:51:13 +0530

    Date Counter

--- CHALLENGE#3/date-counter/src/App.js
+++ CHALLENGE#3/date-counter/src/App.js
@@ -1,27 +1,63 @@
-debugger;
+import React, { useState } from "react";
+
 export default function App() {
+  // const [step, setStep] = useState(1);
+
   return (
     <div className="App">
-      <DateCounter />
+      {/* <StepCounter
+        step={step}
+        onStepIncrement={handleStepIncrement}
+        onStepDecrement={handleStepDecrement}
+      /> */}
+      <StepCounter />
+      <CountCounter />
     </div>
   );
 }
 
-const DateCounter = () => {
-  let step=1
+// const StepCounter = ({ step, onStepIncrement, onStepDecrement }) => {
+const StepCounter = () => {
+  const [step, setStep] = useState(1);
+
+  const onStepIncrement = () => {
+    setStep((forwardStep) => forwardStep + step);
+    console.log("clicked");
+  };
+
+  const onStepDecrement = () => {
+    setStep((prevStep) => prevStep - step);
+    console.log("clicked");
+  };
+
   return (
     <div>
       <div>
-        <button onClick={step}>-</button>
-        <span>Step:1</span>
-        <button>+</button>
-      </div>
-      <div>
-        <button>-</button>
-        <span>Count:1</span>
-        <button>+</button>
+        <button onClick={onStepDecrement}>-</button>
+        <span>Step: {step}</span>
+        <button onClick={onStepIncrement}>+</button>
       </div>
     </div>
   );
 };
-const step = () => {};
+
+const CountCounter = () => {
+  const [count, setCount] = useState(0);
+  const handleCountDecrement = () => {
+    setCount((c) => c - 1);
+    console.log("clicked");
+  };
+  const handleCountIncrement = () => {
+    setCount((c) => c + 1);
+    console.log("clicked");
+  };
+  return (
+    <div>
+      <div>
+        <button onClick={handleCountDecrement}>-</button>
+        <span>Count: {count}</span>
+        <button onClick={handleCountIncrement}>+</button>
+      </div>
+    </div>
+  );
+};
