b8146342f21f938232f8eb54f21be242e3fbc930
ayan878 <ayanraza197@gmail.com>
2024-01-07 13:12:35 +0530

    FlashCards

--- EXERCISE#1/flashcards/src/App.js
+++ EXERCISE#1/flashcards/src/App.js
@@ -1,4 +1,4 @@
-import "./styles.css";
+import React, { useState } from "react";
 
 export default function App() {
   return (
@@ -43,5 +43,23 @@ const questions = [
 ];
 
 function FlashCards() {
-  return <div>TODO</div>;
+  const [selectedId, setSelectedId] = useState(null);
+  function handleClick(id) {
+    setSelectedId(id !== selectedId ? id : null);
+  }
+  return (
+    <div className="flashcards">
+      {questions.map((question) => (
+        <div
+          key={question.id}
+          className={question.id === selectedId ? "selected" : ""}
+          onClick={() => handleClick(question.id)}
+        >
+          <p>
+            {question.id === selectedId ? question.answer : question.question}
+          </p>
+        </div>
+      ))}
+    </div>
+  );
 }
