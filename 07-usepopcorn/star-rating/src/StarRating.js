2b11002e79dfcedf1e796003511c3a817236bc51
ayan878 <ayanraza197@gmail.com>
2024-01-12 22:01:38 +0530

    Star Rating

--- 07-usepopcorn/star-rating/src/StarRating.js
+++ 07-usepopcorn/star-rating/src/StarRating.js
@@ -1,3 +1,25 @@
-export default function StarRating(){
-    return <div><div>{Array.from ({length:5}, (_,i)=><span>S{i}</span>)}</div></div>
+const containerStyle = {
+  display: "flex",
+  alignItems: "center",
+  gap: "16px",
+};
+const starContainerStyle = {
+  display: "flex",
+  gap: "4px",
+};
+const textStyle = {
+  lineHeight: "1",
+  margin: "0",
+};
+export default function StarRating() {
+  return (
+    <div style={containerStyle}>
+      <div style={starContainerStyle}>
+        {Array.from({ length: 5 }, (_, i) => (
+          <span>S{i}</span>
+        ))}
+      </div>
+      <p style={textStyle}>10</p>
+    </div>
+  );
 }
