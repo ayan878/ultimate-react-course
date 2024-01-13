0e49cbb3ec0b733ae7b8edd885beeee3fad72b2b
ayan878 <ayanraza197@gmail.com>
2024-01-13 23:56:40 +0530

    Guess Number

--- EXERCISE#1/guess-number/src/App.js
+++ EXERCISE#1/guess-number/src/App.js
@@ -1,13 +1,27 @@
 import { useState, useEffect } from "react";
-
+// import "/.index.css";
 function App() {
   const [inputValue, setInputValue] = useState(0);
   const [guessedNumber, setGuessedNumber] = useState("?");
   const [pageLoaded, setPageLoaded] = useState(false);
+  const [paragraphValue, setParagraphValue] = useState("Start guessing...");
+  const [bgColor, setBgColor] = useState("bgColor");
+
+  const [score, setScore] = useState(20);
 
   const onClickCheckButton = () => {
     if (parseInt(inputValue, 10) === guessedNumber) {
       setPageLoaded(true);
+      setParagraphValue("🎉 Correct number!");
+      setBgColor("setBgColor");
+    }
+    if (parseInt(inputValue, 10) > guessedNumber) {
+      setScore(score - 1);
+      setParagraphValue("📈 Too High!");
+    }
+    if (parseInt(inputValue, 10) < guessedNumber) {
+      setParagraphValue("📉 Too low!");
+      setScore(score - 1);
     }
   };
 
@@ -20,15 +34,12 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
+    <div className={bgColor}>
       <header>
         <h1>Guess My Number!</h1>
         <p className="between">(Between 1 and 20)</p>
         <button className="btn again">Again!</button>
-        <div
-          className="number"
-          value={guessedNumber}
-        >
+        <div className="number" value={guessedNumber}>
           {pageLoaded ? guessedNumber : "?"}
         </div>
       </header>
@@ -45,9 +56,11 @@ function App() {
           </button>
         </section>
         <section className="right">
-          <p className="message">Start guessing...</p>
+          <p className="message" onClick={onClickCheckButton}>
+            {paragraphValue}
+          </p>
           <p className="label-score">
-            💯 Score: <span className="score">20</span>
+            💯 Score: <span className="score">{score}</span>
           </p>
           <p className="label-highscore">
             🥇 Highscore: <span className="highscore">0</span>
