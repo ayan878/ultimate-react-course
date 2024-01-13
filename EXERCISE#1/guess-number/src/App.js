544b2a662ef26d05796f696750f0317f4d1dd58d
ayan878 <ayanraza197@gmail.com>
2024-01-13 21:32:37 +0530

    Guess Number

--- EXERCISE#1/guess-number/src/App.js
+++ EXERCISE#1/guess-number/src/App.js
@@ -1,27 +1,56 @@
+import { useState, useEffect } from "react";
+
 function App() {
-  let  number=20;
-  let secretNumber = Math.trunc(Math.random() * 20) + 1;
-  function
+  const [inputValue, setInputValue] = useState(0);
+  const [guessedNumber, setGuessedNumber] = useState("?");
+  const [pageLoaded, setPageLoaded] = useState(false);
+
+  const onClickCheckButton = () => {
+    if (parseInt(inputValue, 10) === guessedNumber) {
+      setPageLoaded(true);
+    }
+  };
+
+  const generateRandomNumber = () => {
+    return Math.trunc(Math.random() * 20) + 1;
+  };
+
+  useEffect(() => {
+    setGuessedNumber(generateRandomNumber());
+  }, []);
+
   return (
     <div className="App">
       <header>
         <h1>Guess My Number!</h1>
-        <p class="between">(Between 1 and 20)</p>
-        <button class="btn again">Again!</button>
-        <div class="number">?</div>
+        <p className="between">(Between 1 and 20)</p>
+        <button className="btn again">Again!</button>
+        <div
+          className="number"
+          value={guessedNumber}
+        >
+          {pageLoaded ? guessedNumber : "?"}
+        </div>
       </header>
       <main>
-        <section class="left">
-          <input type="number" class="guess" />
-          <button class="btn check">Check!</button>
+        <section className="left">
+          <input
+            type="number"
+            className="guess"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+          <button className="btn check" onClick={onClickCheckButton}>
+            Check!
+          </button>
         </section>
-        <section class="right">
-          <p class="message">Start guessing...</p>
-          <p class="label-score">
-            💯 Score: <span class="score">20</span>
+        <section className="right">
+          <p className="message">Start guessing...</p>
+          <p className="label-score">
+            💯 Score: <span className="score">20</span>
           </p>
-          <p class="label-highscore">
-            🥇 Highscore: <span class="highscore">0</span>
+          <p className="label-highscore">
+            🥇 Highscore: <span className="highscore">0</span>
           </p>
         </section>
       </main>
