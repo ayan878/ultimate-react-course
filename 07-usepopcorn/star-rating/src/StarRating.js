3fc8746e90fca6aae4960f45df350189bf89ef5f
ayan878 <ayanraza197@gmail.com>
2024-01-14 18:58:04 +0530

    Star Rating

--- 07-usepopcorn/star-rating/src/StarRating.js
+++ 07-usepopcorn/star-rating/src/StarRating.js
@@ -9,13 +9,24 @@ const starContainerStyle = {
   display: "flex",
   gap: "4px",
 };
-const textStyle = {
-  lineHeight: "1",
-  margin: "0",
-};
-export default function StarRating({ maxRating }) {
+
+export default function StarRating({
+  maxRating,
+  color = "#fcc419",
+  size = "48px",
+  className = "",
+  message = [],
+}) {
+  const textStyle = {
+    lineHeight: "1",
+    margin: "0",
+    color,
+    fontSize: `&{parseInt{(size/10)/1.5}px`,
+    display:"block",
+  };
   const [rating, setRating] = useState(0);
   const [tempRating, setTempRating] = useState(0);
+ 
   return (
     <div style={containerStyle}>
       <div style={starContainerStyle}>
@@ -26,15 +37,28 @@ export default function StarRating({ maxRating }) {
             onHoverIn={() => setTempRating(i + 1)}
             onHoverOut={() => setTempRating(0)}
             full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
+            color={color}
+            size={size}
           />
         ))}
       </div>
-      <p style={textStyle}>{tempRating || rating || ""}</p>
+      <p style={textStyle}>
+        {message.length === maxRating
+          ? message[tempRating ? tempRating - 1 : rating - 1]
+          : tempRating || rating || ""}
+      </p>
+      <p style={textStyle}>This movie was rated {rating} stars</p>
     </div>
   );
 }
 
-function Star({ onRate, full, onHoverIn, onHoverOut }) {
+function Star({ onRate, full, onHoverIn, onHoverOut, color, size }) {
+  const starStyle = {
+    display: "block",
+    width: `${size}`,
+    height: `${size}`,
+    cursor: "pointer",
+  };
   return (
     <span
       role="button"
@@ -45,9 +69,9 @@ function Star({ onRate, full, onHoverIn, onHoverOut }) {
       {full ? (
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          fill="fill"
+          fill={color}
           viewBox="0 0 24 24"
-          stroke="#000"
+          stroke={color}
           width="20px"
           height="20px"
           cursor="pointer"
@@ -64,7 +88,7 @@ function Star({ onRate, full, onHoverIn, onHoverOut }) {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke="#000"
+          stroke={color}
           width="20px"
           height="20px"
           cursor="pointer"
