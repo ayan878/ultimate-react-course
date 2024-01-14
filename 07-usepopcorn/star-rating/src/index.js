15c591628d86969ef01ed8083c9a5fb69e8e9c61
ayan878 <ayanraza197@gmail.com>
2024-01-14 18:58:29 +0530

    Star Rating

--- 07-usepopcorn/star-rating/src/index.js
+++ 07-usepopcorn/star-rating/src/index.js
@@ -5,7 +5,7 @@ import StarRating from "./StarRating";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <StarRating  maxRating={5}/>
-    {/* <StarRating maxRating={10} /> */}
+    <StarRating  maxRating={5} message={["Terrible","Bad","Okay","Good","Amazing"]}/>
+    <StarRating  maxRating={10} size={24} color="black" className="text"/>
   </React.StrictMode>
 );
