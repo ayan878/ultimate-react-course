5dac1098ece79384fa177fc798fae74daacfdf49
ayan878 <ayanraza197@gmail.com>
2024-01-17 01:16:53 +0530

    Rock Paper Scissors

--- EXERCISE#1/rock-paper-scissors/src/App.js
+++ EXERCISE#1/rock-paper-scissors/src/App.js
@@ -1,70 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
   return (
-    <div>
-      <div class="container">
-        <header class="header">
-          <img src="./images/logo.svg" alt="logo" class="logo" />
-          <div class="score">
-            <div class="score__title">score</div>
-            <div class="score__number">0</div>
+    <div className="preload">
+      <div className="container">
+        <header className="header">
+          <img src="./images/logo.svg" alt="logo" className="logo" />
+          <div className="score">
+            <div className="score__title">score</div>
+            <div className="score__number">0</div>
           </div>
         </header>
-        <section class="game">
-          <button class="choice-btn" data-choice="paper">
-            <div class="choice paper">
+        <section className="game">
+          <button className="choice-btn" data-choice="paper">
+            <div className="choice paper">
               <img src="./images/icon-paper.svg" alt="Paper" />
             </div>
           </button>
-          <button class="choice-btn" data-choice="scissors">
-            <div class="choice scissors">
+          <button className="choice-btn" data-choice="scissors">
+            <div className="choice scissors">
               <img src="./images/icon-scissors.svg" alt="Scissors" />
             </div>
           </button>
-          <button class="choice-btn" data-choice="rock">
-            <div class="choice rock">
+          <button className="choice-btn" data-choice="rock">
+            <div className="choice rock">
               <img src="./images/icon-rock.svg" alt="Rock" />
             </div>
           </button>
         </section>
       </div>
 
-      <section class="results hidden">
-        <h2 class="results__heading">you picked</h2>
-        <h2 class="results__heading">the house picked</h2>
-        <div class="results__result"></div>
-        <div class="results__winner hidden">
-          <h3 class="results__text">{}</h3>
-          <button class="play-again">play again</button>
+      <section className="results hidden">
+        <h2 className="results__heading">you picked</h2>
+        <h2 className="results__heading">the house picked</h2>
+        <div className="results__result"></div>
+        <div className="results__winner hidden">
+          <h3 className="results__text">{}</h3>
+          <button className="play-again">play again</button>
         </div>
-        <div class="results__result"></div>
+        <div className="results__result"></div>
       </section>
-
-      <button class="rules-btn" tabindex="1">
-        rules
-      </button>
-      <div class="modal">
-        <div class="modal__container">
-          <header class="modal__header">
-            <h2 class="modal__heading">rules</h2>
-            <button class="close-btn">
-              <img src="./images/icon-close.svg" alt="Close Button" />
-            </button>
-          </header>
-          <img
-            src="./images/image-rules.svg"
-            alt="RulesImage"
-            class="rules-img"
-          />
-        </div>
-      </div>
-
-      <footer class="footer">
-        <div class="attribution"></div>
+      <Modal/>
+      <footer className="footer">
+        <div className="attribution"></div>
       </footer>
     </div>
   );
 }
 
 export default App;
+
+function Modal() {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleOpenRulesModal = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseRulesModal = () => {
+    setShowModal(false);
+  };
+  return (
+    <>
+      <button className="rules-btn" tabIndex="1" onClick={handleOpenRulesModal}>
+        rules
+      </button>
+      {showModal && (
+        <div className="modal">
+          <div className="modal__container">
+            <header className="modal__header">
+              <h2 className="modal__heading">rules</h2>
+              <button className="close-btn" onClick={handleCloseRulesModal}>
+                <img src="./images/icon-close.svg" alt="Close Button" />
+              </button>
+            </header>
+            <img
+              src="./images/image-rules.svg"
+              alt="RulesImage"
+              className="rules-img"
+            />
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
