b9e29a87df1f7ee26dfa71a75e2345d745e55fa5
ayan878 <ayanraza197@gmail.com>
2024-01-16 23:25:52 +0530

    Rock Paper Scissors

--- EXERCISE#1/rock-paper-scissors/src/App.js
+++ EXERCISE#1/rock-paper-scissors/src/App.js
@@ -2,45 +2,66 @@ import React from "react";
 
 function App() {
   return (
-    <div className="container">
-      <header className="header">
-        <img src="./images/logo.svg" alt="logo" className="logo" />
-        <div className="score">
-          <div className="score__title">score</div>
-          <div className="score__number">0</div>
+    <div>
+      <div class="container">
+        <header class="header">
+          <img src="./images/logo.svg" alt="logo" class="logo" />
+          <div class="score">
+            <div class="score__title">score</div>
+            <div class="score__number">0</div>
+          </div>
+        </header>
+        <section class="game">
+          <button class="choice-btn" data-choice="paper">
+            <div class="choice paper">
+              <img src="./images/icon-paper.svg" alt="Paper" />
+            </div>
+          </button>
+          <button class="choice-btn" data-choice="scissors">
+            <div class="choice scissors">
+              <img src="./images/icon-scissors.svg" alt="Scissors" />
+            </div>
+          </button>
+          <button class="choice-btn" data-choice="rock">
+            <div class="choice rock">
+              <img src="./images/icon-rock.svg" alt="Rock" />
+            </div>
+          </button>
+        </section>
+      </div>
+
+      <section class="results hidden">
+        <h2 class="results__heading">you picked</h2>
+        <h2 class="results__heading">the house picked</h2>
+        <div class="results__result"></div>
+        <div class="results__winner hidden">
+          <h3 class="results__text">{}</h3>
+          <button class="play-again">play again</button>
         </div>
-      </header>
-      <section className="results hidden">
-        <h2 className="results__heading">you picked</h2>
-        <h2 className="results__heading">the house picked</h2>
-        <div className="results__result"></div>
-        <div className="results__winner hidden">
-          <h3 className="results__text">{}</h3>
-          <button className="play-again">play again</button>
-        </div>
-        <div className="results__result"></div>
+        <div class="results__result"></div>
       </section>
 
-      <button className="rules-btn" tabindex="1">
+      <button class="rules-btn" tabindex="1">
         rules
       </button>
-      <div className="modal">
-        <div className="modal__container">
-          <header className="modal__header">
-            <h2 className="modal__heading">rules</h2>
-            <button className="close-btn">
+      <div class="modal">
+        <div class="modal__container">
+          <header class="modal__header">
+            <h2 class="modal__heading">rules</h2>
+            <button class="close-btn">
               <img src="./images/icon-close.svg" alt="Close Button" />
             </button>
           </header>
           <img
             src="./images/image-rules.svg"
-            alt="RulesImage" className="rules-img"
+            alt="RulesImage"
+            class="rules-img"
           />
         </div>
       </div>
 
-      <footer className="footer">
-        <div className="attribution"></div>
+      <footer class="footer">
+        <div class="attribution"></div>
       </footer>
     </div>
   );
