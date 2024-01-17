218a88a75d59a835b377dc7fe71d5a1c66aeaf53
ayan878 <ayanraza197@gmail.com>
2024-01-17 12:35:09 +0530

    Rock Paper Scissors

--- EXERCISE#1/rock-paper-scissors/src/App.js
+++ EXERCISE#1/rock-paper-scissors/src/App.js
@@ -1,8 +1,22 @@
 import React, { useState } from "react";
 
-function App() {
+export default function App() {
+  const [showResult, setShowResult] = useState(false);
+  const [showModal, setShowModal] = useState(false);
   return (
     <div className="preload">
+      <Header />
+      <Game setShowResult={setShowResult} showResult={showResult} />
+      <Result showResult={showResult} setShowResult={setShowResult} />
+      <Modal setShowModal={setShowModal} showModal={showModal} />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <>
       <div className="container">
         <header className="header">
           <img src="./images/logo.svg" alt="logo" className="logo" />
@@ -11,48 +25,79 @@ function App() {
             <div className="score__number">0</div>
           </div>
         </header>
-        <section className="game">
-          <button className="choice-btn" data-choice="paper">
-            <div className="choice paper">
-              <img src="./images/icon-paper.svg" alt="Paper" />
-            </div>
-          </button>
-          <button className="choice-btn" data-choice="scissors">
-            <div className="choice scissors">
-              <img src="./images/icon-scissors.svg" alt="Scissors" />
-            </div>
-          </button>
-          <button className="choice-btn" data-choice="rock">
-            <div className="choice rock">
-              <img src="./images/icon-rock.svg" alt="Rock" />
-            </div>
-          </button>
-        </section>
       </div>
+    </>
+  );
+}
+function Game({ setShowResult, showResult }) {
+  const choices = ["rock", "paper", "scissors"];
+  const computerChoice = choices[Math.floor(Math.random() * choices.length)];
 
-      <section className="results hidden">
-        <h2 className="results__heading">you picked</h2>
-        <h2 className="results__heading">the house picked</h2>
-        <div className="results__result"></div>
-        <div className="results__winner hidden">
-          <h3 className="results__text">{}</h3>
-          <button className="play-again">play again</button>
-        </div>
-        <div className="results__result"></div>
-      </section>
-      <Modal/>
-      <footer className="footer">
-        <div className="attribution"></div>
-      </footer>
-    </div>
+  const handleChoiceSelection = (userChoice) => {
+    console.log(`You selected: ${userChoice}`);
+    console.log(`Computer selected: ${computerChoice}`);
+    playRound(computerChoice, userChoice);
+    setShowResult(true);
+  };
+
+  function playRound(computerChoice, userSelection) {
+    if (userSelection === "rock" && computerChoice === "scissors") {
+    } else if (userSelection === "paper" && computerChoice === "rock") {
+    } else if (userSelection === "scissors" && computerChoice === "paper") {
+    } else if (computerChoice === "rock" && userSelection === "rock") {
+    } else if (computerChoice === "paper" && userSelection === "paper") {
+      alert("It/s a tie!");
+    } else if (computerChoice === "scissors" && userSelection === "scissors") {
+    } else {
+      alert("You LOSE!, Your Score Is: ");
+    }
+  }
+  return (
+    <>
+      {!showResult && (
+        <section className="game">
+          {/* <button className="choice-btn" data-choice="paper">
+          <div className="choice paper">
+            <img src="./images/icon-paper.svg" alt="Paper" />
+          </div>
+        </button>
+        <button className="choice-btn" data-choice="scissors">
+          <div className="choice scissors">
+            <img src="./images/icon-scissors.svg" alt="Scissors" />
+          </div>
+        </button>
+        <button className="choice-btn" data-choice="rock">
+          <div className="choice rock">
+            <img src="./images/icon-rock.svg" alt="Rock" />
+          </div>
+        </button> */}
+          <ChoiceButton
+            choice="paper"
+            onClick={() => handleChoiceSelection("paper")}
+          >
+            <img src="./images/icon-paper.svg" alt="Paper" />
+          </ChoiceButton>
+
+          <ChoiceButton
+            choice="scissors"
+            onClick={() => handleChoiceSelection("scissors")}
+          >
+            <img src="./images/icon-scissors.svg" alt="Scissors" />
+          </ChoiceButton>
+
+          <ChoiceButton
+            choice="rock"
+            onClick={() => handleChoiceSelection("rock")}
+          >
+            <img src="./images/icon-rock.svg" alt="Rock" />
+          </ChoiceButton>
+        </section>
+      )}
+    </>
   );
 }
 
-export default App;
-
-function Modal() {
-  const [showModal, setShowModal] = useState(false);
-
+function Modal({ showModal, setShowModal }) {
   const handleOpenRulesModal = () => {
     setShowModal(true);
   };
@@ -85,3 +130,43 @@ function Modal() {
     </>
   );
 }
+function Result({ showResult, setShowResult }) {
+  return (
+    <>
+      {showResult && (
+        <section className="results">
+          <h2 className="results__heading">you picked</h2>
+          <h2 className="results__heading">the house picked</h2>
+          <div className="results__result"></div>
+          <div className="results__winner hidden">
+            <h3 className="results__text">{}</h3>
+            <button
+              className="play-again"
+              onClick={() => {
+                setShowResult(false);
+                alert("dfkjsd");
+              }}
+            >
+              play again
+            </button>
+          </div>
+          <div className="results__result"></div>
+        </section>
+      )}
+    </>
+  );
+}
+function Footer() {
+  return (
+    <div>
+      <footer className="footer">
+        <div className="attribution"></div>
+      </footer>
+    </div>
+  );
+}
+const ChoiceButton = ({ choice, children, onClick }) => (
+  <button className={`choice-btn`} data-choice={choice} onClick={onClick}>
+    <div className={`choice ${choice}`}>{children}</div>
+  </button>
+);
