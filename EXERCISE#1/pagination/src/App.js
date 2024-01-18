3613ff0cec5fe2e9919c41a9286eca63d287513a
ayan878 <ayanraza197@gmail.com>
2024-01-19 01:05:03 +0530

    Pagination

--- EXERCISE#1/pagination/src/App.js
+++ EXERCISE#1/pagination/src/App.js
@@ -1,27 +1,36 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa6";
+
 function App() {
   const [page, setPage] = useState(1);
   const pageLinks = Array.from({ length: 12 }, (_, i) => i + 1);
+  const anchorRefs = pageLinks.map(() => useRef(null));
 
   const handleNext = () => {
-    if (page < 23) {
+    if (page < 12) {
       setPage((prevPage) => prevPage + 1);
     }
   };
+
   const handlePrev = () => {
-    if (page > 23) {
-      setPage((nextPage) => nextPage + 1);
+    if (page > 1) {
+      setPage((prevPage) => prevPage - 1);
     }
   };
+
+  useEffect(() => {
+    anchorRefs[page - 1].current.focus();
+  }, [page]);
+
   return (
     <>
       <div className="pagination">
         <button className="btn" onClick={handlePrev}>
           <FaChevronLeft />
         </button>
-        {pageLinks.map((link) => (
+        {pageLinks.map((link, index) => (
           <a
+            ref={anchorRefs[index]}
             href={`#page-link-${link}`}
             id={`page-link-${link}`}
             className="page-link"
