2d2539181ec0def31d468bc84e98c9a03910f0c4
ayan878 <ayanraza197@gmail.com>
2024-01-18 23:20:50 +0530

    Pagination

--- EXERCISE#1/pagination/src/App.js
+++ EXERCISE#1/pagination/src/App.js
@@ -1,5 +1,52 @@
+import { useState } from "react";
+import { FaLessThan, FaGreaterThan } from "react-icons/fa6";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="container">
+      <button className="previous-btn">
+        <FaLessThan />
+      </button>
+      <a href="#" className="page-link">
+        1
+      </a>
+      <a href="#" className="page-link">
+        2
+      </a>
+      <a href="#" className="page-link">
+        3{" "}
+      </a>
+      <a href="#" className="page-link">
+        4
+      </a>
+      <a href="#" className="page-link">
+        5
+      </a>
+      <a href="#" className="page-link">
+        6
+      </a>
+      <a href="#" className="page-link">
+        7
+      </a>
+      <a href="#" className="page-link">
+        .
+      </a>
+      <a href="#" className="page-link">
+        .
+      </a>
+      <a href="#" className="page-link">
+        .
+      </a>
+      <a href="#" className="page-link">
+        .
+      </a>
+      <a href="#" className="page-link">
+        23
+      </a>
+      <button className="next-btn">
+        <FaGreaterThan />
+      </button>
+    </div>
+  );
 }
 
 export default App;
