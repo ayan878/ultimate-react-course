2b14004628e3f3dc58758f4438cf6694448b2e4b
ayan878 <ayanraza197@gmail.com>
2024-01-19 22:59:06 +0530

    Pagination

--- EXERCISE#1/pagination/src/App.js
+++ EXERCISE#1/pagination/src/App.js
@@ -1,47 +1,62 @@
-import { useState, useRef } from "react";
+import React, { useState } from "react";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa6";
 
 function App() {
   const [page, setPage] = useState(1);
-  const pageLinks = Array.from({ length: 12 }, (_, i) => i + 1);
-  // const anchorRefs = pageLinks.map(() => useRef(null));
+  const totalPageCount = 12; // Assuming 12 pages
+  const pageLinks = Array.from({ length: totalPageCount }, (_, i) => i + 1);
+
+  const updatePagination = () => {
+    const pageLinksContainer = document.getElementById("page-links-container");
+    const pageLinks = pageLinksContainer.getElementsByClassName("page-link");
+
+    Array.from(pageLinks).forEach((link, index) => {
+      const pageNumber = index + 1;
+      link.classList.toggle("active", pageNumber === page);
+    });
+  };
 
   const handleNext = () => {
-    if (page < 12) {
+    if (page < totalPageCount) {
       setPage((prevPage) => prevPage + 1);
-      // anchorRefs[page].current.click();
+      updatePagination();
     }
   };
 
   const handlePrev = () => {
     if (page > 1) {
       setPage((prevPage) => prevPage - 1);
-      // anchorRefs[page - 2].current.click();
+      updatePagination();
     }
   };
 
   return (
     <>
       <div className="pagination">
-        <button className="btn" onClick={handlePrev}>
+        <button className="btn" onClick={handlePrev} disabled={page === 1}>
           <FaChevronLeft />
         </button>
-        {pageLinks.map((link, index) => (
-          <a
-            // ref={anchorRefs[index]}
-            href={`#page-link-${link}`}
-            id={`page-link-${link}`}
-            className="page-link"
-            key={link}
-          >
-            {link}
-          </a>
-        ))}
-        <button className="btn" onClick={handleNext}>
+          {pageLinks.map((link) => (
+            <a
+              href={`#page-link-${link}`}
+              id={`page-link-${link}`}
+              className={`page-link ${link === page ? "active" : ""}`}
+              key={link}
+            >
+              {link}
+            </a>
+          ))}
+
+        <button
+          className="btn"
+          onClick={handleNext}
+          disabled={page === totalPageCount}
+        >
           <FaChevronRight />
         </button>
       </div>
     </>
   );
 }
+
 export default App;
