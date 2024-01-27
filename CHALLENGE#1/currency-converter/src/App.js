df004e377e414a1a6083695992b149215533e2fd
ayan878 <ayanraza197@gmail.com>
2024-01-27 20:21:38 +0530

    currency converter

---
+++ CHALLENGE#1/currency-converter/src/App.js
@@ -0,0 +1,23 @@
+// `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
+
+function App() {
+  return (
+    <div>
+      <select>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <select>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <p>{}</p>
+    </div>
+  );
+}
+
+export default App;
