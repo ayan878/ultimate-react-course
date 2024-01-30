7a109d09e36c7e6e6679d5664a9457e1d0087224
ayan878 <ayanraza197@gmail.com>
2024-01-30 23:56:41 +0530

    react-quiz

--- 10-react-quiz/react-quiz/src/component/App.js
+++ 10-react-quiz/react-quiz/src/component/App.js
@@ -5,12 +5,15 @@ import Loader from "./Loader.js";
 import Error from "./Error.js";
 import StartScreen from "./StartScreen.js";
 import Questions from "./Questions.js";
+import NextButton from "./NextButton.js";
 
 function initialState() {
   return {
     questions: [],
     status: "loading",
     index: 0,
+    answer: null,
+    points: 0,
   };
 }
 
@@ -23,6 +26,18 @@ function reducer(state, action) {
       return { ...state, status: "error" };
     case "start":
       return { ...state, status: "active" };
+    case "newAnswer":
+      const question = state.questions[state.index];
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
+    case "nextQuestion":
+      return { ...state, index: state.index + 1,answer:null };
 
     default:
       throw new Error("Action unknown");
@@ -30,10 +45,10 @@ function reducer(state, action) {
 }
 
 function App() {
-  const [{ questions, status, index }, dispatch] = useReducer(
+  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
     reducer,
     initialState()
-  ); // Initialize with initialState instead of 0
+  );
   const numQuestions = questions.length;
   useEffect(() => {
     fetch("http://localhost:8000/questions")
@@ -56,7 +71,16 @@ function App() {
         {status === "ready" && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-      {status === "active" && <Questions question={questions[index]} />}
+        {status === "active" && (
+          <>
+          <Questions
+            question={questions[index]}
+            dispatch={dispatch}
+            answer={answer}
+          />
+          <NextButton dispatch={dispatch} answer={answer}/>
+          </>
+        )}
       </Main>
     </div>
   );
