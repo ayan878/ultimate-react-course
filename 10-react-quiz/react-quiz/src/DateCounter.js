a77e0fcb0edc626f2f69d3173c04c668de7080a4
ayan878 <ayanraza197@gmail.com>
2024-01-30 12:55:17 +0530

    react-quiz

--- 10-react-quiz/react-quiz/src/DateCounter.js
+++ 10-react-quiz/react-quiz/src/DateCounter.js
@@ -2,9 +2,11 @@ import { useReducer, useState } from "react";
 
 function reducer(state, action) {
   console.log(state, action);
-  return state + action;
+  // return state + action;
+  if(action.type==="inc")return state + action.payload;
+   if(action.type==="dec")return state - action.payload;
+    if(action.type==="setCount")return action.payload;
 }
-
 function DateCounter() {
   // const [count, setCount] = useState(0);
   const [count, dispatch] = useReducer(reducer, 0);
@@ -15,19 +17,19 @@ function DateCounter() {
   date.setDate(date.getDate() + count);
 
   const dec = function () {
-    dispatch(-1);
+    dispatch({type:"dec",payload:-1});
     // setCount((count) => count - 1);
     // setCount((count) => count - step);
   };
 
   const inc = function () {
-    dispatch(1);
+    dispatch({ type: "inc", payload: 1 });
     // setCount((count) => count + 1);
     // setCount((count) => count + step);
   };
 
   const defineCount = function (e) {
-    dispatch(Number(e.target.value));
+    dispatch({type:"setCount",payload:Number(e.target.value)});
     // setCount(Number(e.target.value));
   };
 
