ab7f8527f07c275615d6dbc55ab213eb0ae6bb46
ayan878 <ayanraza197@gmail.com>
2024-02-11 09:49:31 +0530

    Atomic Blog App

--- 12-atomic-blog/starter/src/App-memo.js
+++ 12-atomic-blog/starter/src/App-memo.js
@@ -1,4 +1,4 @@
-import { memo, useCallback, useEffect, useMemo, useState } from "react";
+import { memo, useEffect, useMemo, useState } from "react";
 import { faker } from "@faker-js/faker";
 
 function createRandomPost() {
@@ -40,12 +40,12 @@ function App() {
     },
     [isFakeDark]
   );
-const archiveOption= useMemo(()=>{
-  return {
-  show:false,
-  title:"Post archieve in addtion to main posts",
-}
-})
+  const archiveOption = useMemo(() => {
+    return {
+      show: false,
+      title: `Post archieve in addtion to ${posts.length} main posts`,
+    };
+  }, [posts.length]);
 
   return (
     <section>
@@ -160,18 +160,17 @@ function List({ posts }) {
   );
 }
 
-const Archive= memo( function Archive({ archiveOption }) {
+const Archive = memo(function Archive({ archiveOption }) {
+  const [showArchive, setShowArchive] = useState(archiveOption.show);
   // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
   const [posts] = useState(() =>
     // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
     Array.from({ length: 30000 }, () => createRandomPost())
   );
 
-  const [showArchive, setShowArchive] = useState(archiveOption.show);
-
   return (
     <aside>
-      <h2>Post archive</h2>
+      <h2>{archiveOption.title}</h2>
       <button onClick={() => setShowArchive((s) => !s)}>
         {showArchive ? "Hide archive posts" : "Show archive posts"}
       </button>
@@ -190,7 +189,7 @@ const Archive= memo( function Archive({ archiveOption }) {
       )}
     </aside>
   );
-})
+});
 
 function Footer() {
   return <footer>&copy; by The Atomic Blog ✌️</footer>;
