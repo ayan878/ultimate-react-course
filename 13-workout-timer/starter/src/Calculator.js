dc24980230ef3f13167f308797b80a36bda3c1cb
ayan878 <ayanraza197@gmail.com>
2024-02-12 20:24:41 +0530

    workout timer

--- 13-workout-timer/starter/src/Calculator.js
+++ 13-workout-timer/starter/src/Calculator.js
@@ -8,31 +8,39 @@ function Calculator({ workouts, allowSound }) {
   const [durationBreak, setDurationBreak] = useState(5);
   const [duration, setDuration] = useState(0);
 
-  const playSound = useCallback(
-    function () {
+  // const playSound = useCallback(
+  //   function () {
+  //     if (!allowSound) return;
+  //     const sound = new Audio(clickSound);
+  //     sound.play();
+  //   },
+  //   [allowSound]
+  // );
+
+  // for setting playing sound
+  useEffect(() => {
+    const playSound = function () {
       if (!allowSound) return;
       const sound = new Audio(clickSound);
       sound.play();
-    },
-    [allowSound]
-  );
+    };
+    playSound();
+  }, [duration,allowSound]);
 
+  //for setting the duration
   useEffect(() => {
     setDuration((number * sets * speed) / 60 + (sets - 1) * durationBreak);
-    playSound();
-  }, [number, sets, speed, durationBreak, playSound]);
+  }, [number, sets, speed, durationBreak]);
   // const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
   const mins = Math.floor(duration);
   const seconds = (duration - mins) * 60;
 
   function handleInc() {
     setDuration((duration) => Math.ceil(duration + 1));
-    playSound();
   }
 
   function handleDec() {
     setDuration((duration) => Math.ceil(duration - 1));
-    playSound();
   }
 
   return (
