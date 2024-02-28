779b83faeb04b7dd2b2e54786404da6e6cc09f7d
ayan878 <ayanraza197@gmail.com>
2024-02-29 01:44:24 +0530

    App

--- Mini Proejct/bankist-app/src/App.jsx
+++ Mini Proejct/bankist-app/src/App.jsx
@@ -1,9 +1,28 @@
-import Header from './components/Header'
+import Balance from './components/Balance';
+import CloseAccount from './components/CloseAccount';
+import Header from './components/Header';
+import Loan from './components/Loan';
+import Movement from './components/Movement';
+import Summary from './components/Summary';
+import Transfer from './components/Transfer';
 
 function App() {
   return (
-    <><Header/></>
-  )
+    <>
+      <Header />
+      <main className="app">
+        <Balance />
+        <Movement />
+        <Summary />
+        <Transfer />
+        <Loan />
+        <CloseAccount/>
+        <p className="logout-timer">
+          You will be logged out in <span className="timer">05:00</span>
+        </p>
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
