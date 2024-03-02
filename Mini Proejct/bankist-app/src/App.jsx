1b83ab0cf12bc4780fd9ed90df45864ff4981e57
ayan878 <ayanraza197@gmail.com>
2024-03-02 13:13:26 +0530

    App

--- Mini Proejct/bankist-app/src/App.jsx
+++ Mini Proejct/bankist-app/src/App.jsx
@@ -9,11 +9,12 @@ import Summary from './components/Summary';
 import Transfer from './components/Transfer';
 
 function App() {
-  // const { isAuthenticated } = useAuth();
+  const  isAuthenticated  = useAuth();
 
   return (
     <AuthProvider>
-      <Header /> 
+      <Header />
+      {isAuthenticated && (
         <main className="app">
           <Balance />
           <Movement />
@@ -25,6 +26,7 @@ function App() {
             You will be logged out in <span className="timer">05:00</span>
           </p>
         </main>
+      )}
     </AuthProvider>
   );
 }
