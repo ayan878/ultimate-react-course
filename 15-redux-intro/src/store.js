ea9a5d8ef1809bcce2dc7f8dd6030d6f2dba002f
ayan878 <ayanraza197@gmail.com>
2024-03-04 23:34:11 +0530

    store

--- 15-redux-intro/src/store.js
+++ 15-redux-intro/src/store.js
@@ -1,30 +1,50 @@
 import { createStore } from "redux";
+
 const initialState = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
 };
+
 function reducer(state = initialState, action) {
   switch (action.type) {
-    case "account/deposite":
+    case "account/deposit":
       return { ...state, balance: state.balance + action.payload };
     case "account/withdraw":
       return { ...state, balance: state.balance - action.payload };
     case "account/requestLoan":
       if (state.loan > 0) return state;
-      //later
-      return { ...state, balance: state.balance + action.payload };
+
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+      };
+
     case "account/payLoan":
       return {
         ...state,
         loan: 0,
         loanPurpose: "",
-        balance: state.balance - state.loan,
+        balance: state.balance - action.payload,
       };
     default:
       return state;
   }
 }
+
 const store = createStore(reducer);
-store.dispatch({ type: "account/deposite", payload: 500 });
+store.dispatch({ type: "account/deposit", payload: 500 });
+// console.log(store.getState());
+
+store.dispatch({ type: "account/withdraw", payload: 200 });
+// console.log(store.getState());
+
+store.dispatch({
+  type: "account/requestLoan",
+  payload: { amount: 1000, purpose: "Buy a car" },
+});
+console.log(store.getState());
+
+store.dispatch({ type: "account/patLoan" });
 console.log(store.getState());
