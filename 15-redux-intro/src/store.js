29daaa9265663f65a65d5b51682056f9fce639d6
ayan878 <ayanraza197@gmail.com>
2024-03-05 17:05:29 +0530

    redux

--- 15-redux-intro/src/store.js
+++ 15-redux-intro/src/store.js
@@ -1,13 +1,60 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { createStore } from "redux";
 
-import accountReducer from "./features/accounts/accountSlice";
-import customerReducer from "./features/customers/customerSlice";
+// Define action creators
+function deposite(amount) {
+  return { type: "account/deposit", payload: amount };
+}
 
-const store = configureStore({
-  reducer: {
-    account: accountReducer,
-    customer: customerReducer,
-  },
-});
+function withdraw(amount) {
+  return { type: "account/withdraw", payload: amount };
+}
 
-export default store;
+function requestLoan(amount, purpose) {
+  return { type: "account/requestLoan", payload: { amount, purpose } };
+}
+
+function payLoan(amount) {
+  return { type: "account/payLoan", payload: amount };
+}
+
+// Reducer function
+const initialState = {
+  balance: 0,
+  loan: 0,
+  loanPurpose: "",
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case "account/deposit":
+      return { ...state, balance: state.balance + action.payload };
+    case "account/withdraw":
+      return { ...state, balance: state.balance - action.payload };
+    case "account/requestLoan":
+      if (state.loan > 0) return state;
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+      };
+    case "account/payLoan":
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: "",
+        balance: state.balance - action.payload,
+      };
+    default:
+      return state;
+  }
+}
+
+// Create Redux store
+const store = createStore(reducer);
+
+// Dispatch actions using action creators
+store.dispatch(deposite(500));
+store.dispatch(withdraw(200));
+store.dispatch(requestLoan(1000, "Buy a car"));
+
+console.log(store.getState().balance); // Output: 1300
