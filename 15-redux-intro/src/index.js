e845f46192795f7108e69928fa9df07167ecdbb9
ayan878 <ayanraza197@gmail.com>
2024-03-12 19:11:34 +0530

    React-Redux Bank

--- 15-redux-intro/src/index.js
+++ 15-redux-intro/src/index.js
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 // import './index.css';
-import "./store";
+import store from "./store";
 import App from "./App";
+
+store.dispatch({ type: "account/deposite" });
+console.log(store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
