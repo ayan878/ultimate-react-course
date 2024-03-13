c90f15089734abe863951e9a33e10ed2f02b672f
ayan878 <ayanraza197@gmail.com>
2024-03-13 14:07:00 +0530

    React-Redux Bank

--- 15-redux-intro/src/features/accounts/BalanceDisplay.js
+++ 15-redux-intro/src/features/accounts/BalanceDisplay.js
@@ -1,4 +1,24 @@
-import { useSelector } from "react-redux";
+
+// //by using useSelector
+// import { useSelector } from "react-redux";
+
+// function formatCurrency(value) {
+//   return new Intl.NumberFormat("en", {
+//     style: "currency",
+//     currency: "USD",
+//   }).format(value);
+// }
+
+// function BalanceDisplay() {
+//   const balance = useSelector((store) => store.account.balance);
+//   return <div className="balance">{formatCurrency(parseFloat(balance))}</div>;
+// }
+
+// export default BalanceDisplay;
+
+//by using connect function
+
+import { connect } from "react-redux";
 
 function formatCurrency(value) {
   return new Intl.NumberFormat("en", {
@@ -7,9 +27,14 @@ function formatCurrency(value) {
   }).format(value);
 }
 
-function BalanceDisplay() {
-  const balance = useSelector((store) => store.account.balance);
+function BalanceDisplay({ balance }) {
   return <div className="balance">{formatCurrency(parseFloat(balance))}</div>;
 }
 
-export default BalanceDisplay;
+function mapStateToProps(state) {
+  return {
+    balance: state.account.balance,
+  };
+}
+
+export default connect(mapStateToProps)(BalanceDisplay);
