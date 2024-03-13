6bf0b1723e9eb9202bb8b3dbf391f9f9fff734d3
ayan878 <ayanraza197@gmail.com>
2024-03-13 11:22:20 +0530

    React-Redux Bank

--- 15-redux-intro/src/features/customers/CreateCustomer.js
+++ 15-redux-intro/src/features/customers/CreateCustomer.js
@@ -1,11 +1,16 @@
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { createCustomer } from "./customerSlice";
 
 function Customer() {
   const [fullName, setFullName] = useState("");
   const [nationalId, setNationalId] = useState("");
 
+  const dispatch = useDispatch();
+
   function handleClick() {
-    
+    if (!fullName || !nationalId) return;
+    dispatch(createCustomer({ fullName, nationalId }));
   }
 
   return (
