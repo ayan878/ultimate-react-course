f47e8fcff0ded3685bac4aeb5fc8ade565803aa7
ayan878 <ayanraza197@gmail.com>
2024-03-14 23:08:35 +0530

    React-Redux Bank

--- 15-redux-intro/src/store.js
+++ 15-redux-intro/src/store.js
@@ -1,20 +1,14 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
-import {thunk} from "redux-thunk";
+// import { applyMiddleware, combineReducers, createStore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
 
-// Combine reducers
-const rootReducer = combineReducers({
-  account: accountReducer,
-  customer: customerReducer,
+const store=configureStore({
+  reducer: {
+    account:accountReducer,
+    customer:customerReducer,
+  },
 });
-
-// Create Redux store with middleware (Thunk)
-const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
 
-// In order to use middleware (Thunk) we have to follow Three Step
-// Step:1 install middleware package
-// step :2 we apply meddleware to store
-// step :3 We use middleware in action creator function
