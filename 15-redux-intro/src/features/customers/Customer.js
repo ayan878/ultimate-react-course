f0caef2ee2c98eddae6576010cf58890202ff819
ayan878 <ayanraza197@gmail.com>
2024-03-15 22:27:49 +0530

    React-Redux Bank

--- 15-redux-intro/src/features/customers/Customer.js
+++ 15-redux-intro/src/features/customers/Customer.js
@@ -1,11 +1,10 @@
 import { useSelector } from "react-redux";
 
-
 function Customer() {
   const customer = useSelector((store) => store.customer);
-  const {fullName}=customer.fullName
+  const { fullName } = customer;
   console.log(fullName);
-  
+
   return <h2>👋 Welcome, {fullName}</h2>;
 }
 
