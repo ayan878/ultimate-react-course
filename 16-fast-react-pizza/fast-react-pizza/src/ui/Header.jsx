36d5b2268fb0dc7bb4cd2b6c47d4e9f4ba32e834
ayan878 <ayanraza197@gmail.com>
2024-03-18 12:58:33 +0530

    header

---
+++ 16-fast-react-pizza/fast-react-pizza/src/ui/Header.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <Header>
+      <Link to="/">Fast React Pizza Co.</Link>
+    </Header>
+  );
+}
+
+export default Header;
