ff1b553019b15f689f64ce603f49a640cb7f4817
ayan878 <ayanraza197@gmail.com>
2024-03-19 23:15:02 +0530

    app

--- 16-fast-react-pizza/fast-react-pizza/src/App.jsx
+++ 16-fast-react-pizza/fast-react-pizza/src/App.jsx
@@ -6,21 +6,34 @@ import CreateOrder from "./features/order/CreateOrder";
 import Order from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
 
-const router = createBrowserRouter({
-  routes: [
-    {
-      path: "/",
-      element: <AppLayout />,
-      children: [
-        { path: "/home", element: <Home /> },
-        { path: "/menu", element: <Menu /> },
-        { path: "/cart", element: <Cart /> },
-        { path: "/order/new", element: <CreateOrder /> },
-        { path: "/order/:orderId", element: <Order /> },
-      ],
-    },
-  ],
-});
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+      },
+      {
+        path: "/order/orderId",
+        element: <Order/>,
+      },
+    ],
+  },
+]);
+
 
 function App() {
   return <RouterProvider router={router} />;
