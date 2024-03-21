cdb56406875c9953e192671b322b10b6702b29c1
ayan878 <ayanraza197@gmail.com>
2024-03-21 20:23:56 +0530

    menu

--- 16-fast-react-pizza/fast-react-pizza/src/features/menu/Menu.jsx
+++ 16-fast-react-pizza/fast-react-pizza/src/features/menu/Menu.jsx
@@ -1,13 +1,22 @@
-import { useLoaderData } from 'react-router-dom';
-import {getMenu} from '../../services/apiRestaurant'
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant";
+import MenuItem from "./MenuItem";
+
 function Menu() {
-  const menu= useLoaderData();
+  const menu = useLoaderData();
   console.log(menu);
-  return <h1>Menu</h1>;
+
+  return (
+    <ul>
+      {menu.map((pizza) => (
+        <MenuItem pizza={pizza} key={pizza.id} />
+      ))}
+    </ul>
+  );
 }
 
-export async function loader(){
-  const menu=await getMenu();
+export async function loader() {
+  const menu = await getMenu();
   return menu;
 }
 
