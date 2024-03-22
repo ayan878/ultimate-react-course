7f44a35070cc53b216729a1413695b52b466cd1a
ayan878 <ayanraza197@gmail.com>
2024-03-22 21:24:38 +0530

    order

--- 16-fast-react-pizza/fast-react-pizza/src/features/order/Order.jsx
+++ 16-fast-react-pizza/fast-react-pizza/src/features/order/Order.jsx
@@ -1,50 +1,16 @@
-// Test ID: IIDSAT
 import { useLoaderData } from "react-router-dom";
+import { calcMinutesLeft, formatCurrency, formatDate } from "../../utils/helpers";
 import { getOrder } from "../../services/apiRestaurant";
-import {
-  calcMinutesLeft,
-  formatCurrency,
-  formatDate,
-} from "../../utils/helpers";
-
-// const order = {
-//   id: "ABCDEF",
-//   customer: "Jonas",
-//   phone: "123456789",
-//   address: "Arroios, Lisbon , Portugal",
-//   priority: true,
-//   estimatedDelivery: "2027-04-25T10:00:00",
-//   cart: [
-//     {
-//       pizzaId: 7,
-//       name: "Napoli",
-//       quantity: 3,
-//       unitPrice: 16,
-//       totalPrice: 48,
-//     },
-//     {
-//       pizzaId: 5,
-//       name: "Diavola",
-//       quantity: 2,
-//       unitPrice: 16,
-//       totalPrice: 32,
-//     },
-//     {
-//       pizzaId: 3,
-//       name: "Romana",
-//       quantity: 1,
-//       unitPrice: 15,
-//       totalPrice: 15,
-//     },
-//   ],
-//   position: "-9.000,38.000",
-//   orderPrice: 95,
-//   priorityPrice: 19,
-// };
 
 function Order() {
-  const order=useLoaderData()
-  // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
+  const order = useLoaderData();
+  
+
+  if (!order) {
+    // Handle loading state or error state here
+    return <div>Loading...</div>;
+  }
+
   const {
     // id,
     status,
@@ -60,7 +26,6 @@ function Order() {
     <div>
       <div>
         <h2>Status</h2>
-
         <div>
           {priority && <span>Priority</span>}
           <span>{status} order</span>
@@ -70,7 +35,7 @@ function Order() {
       <div>
         <p>
           {deliveryIn >= 0
-            ? `Only ${calcMinutesLeft(estimatedDelivery)} minutes left 😃`
+            ? `Only ${deliveryIn} minutes left 😃`
             : "Order should have arrived"}
         </p>
         <p>(Estimated delivery: {formatDate(estimatedDelivery)})</p>
@@ -84,14 +49,10 @@ function Order() {
     </div>
   );
 }
-export async function loader(params) {
-  if (!params || !params.orderId) {
-    throw new Error("Invalid parameters: orderId is missing.");
-  }
 
+export async function loader({ params }) {
   const order = await getOrder(params.orderId);
   return order;
 }
-
 
 export default Order;
