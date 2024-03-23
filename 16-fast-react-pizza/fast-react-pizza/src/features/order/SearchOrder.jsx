bb273ec9db56e463a401335afadda58c7d49d080
ayan878 <ayanraza197@gmail.com>
2024-03-23 12:48:37 +0530

    search-order

--- 16-fast-react-pizza/fast-react-pizza/src/features/order/SearchOrder.jsx
+++ 16-fast-react-pizza/fast-react-pizza/src/features/order/SearchOrder.jsx
@@ -18,7 +18,7 @@ function SearchOrder() {
         onChange={(e) => {
           setQuery(e.target.value);
         }}
-      />
+      className="rounded-full px-4 py-2 text-sm placeholder:text-stone-400 w-28 sm:w-64 sm:focus:w-72 transition-all duration-300 focus:outline-none focus:ring focus:ring-yellow-600 focus:ring-opacity-50"/>
     </form>
   );
 }
